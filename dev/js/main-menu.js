ada0fef1cba2b777647e8fa8734bd32c2eeb8363
FERNANDO <joferguzvas@gmail.com>
2023-03-26 03:30:02 -0600

    menu administrador

--- dev/js/main-menu.js
+++ dev/js/main-menu.js
@@ -1,6 +1,8 @@
 const toggleMenu = document.getElementById("toggle-menu");
 const sidebar = document.getElementById("sidebar");
 
+const opciones = document.querySelectorAll(".opcion");
+
 if (toggleMenu) {
   toggleMenu.addEventListener("click", (e) => {
     if (
@@ -9,6 +11,9 @@ if (toggleMenu) {
     ) {
       sidebar.classList.toggle("show");
       toggleMenu.classList.toggle("show");
+      opciones.forEach((icon) => {
+        icon.removeChild(icon.children[1]);
+      });
     }
   });
 }
